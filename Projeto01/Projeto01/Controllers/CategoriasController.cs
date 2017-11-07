4245f44c196c7aa45d114e2f38ba051546da2495
Aldryn <Capitulo 1>
2017-11-07 10:45:08 -0200

    Capitulo 1

---
+++ Projeto01/Projeto01/Controllers/CategoriasController.cs
@@ -0,0 +1,18 @@
+﻿using Projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+ 
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
